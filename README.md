d13f9bca74b9f156ff09c2e3369d9fff895de29d
Humba01 <humbandroid@gmail.com>
2023-07-21 04:34:11 -0300

    Expandindo projeto

---
+++ README.md
@@ -0,0 +1,1 @@
+# wtsfy-labo
---
+++ app/index.mjs
@@ -0,0 +1,5 @@
+import { wertsfy } from "@humbanew/wertsfy";
+
+wertsfy.Bibliotecas.arithm.Methods.m_arrendonda_para_baixo();
+wertsfy.Bibliotecas.arithm.Consts.c_avogadro;
+wertsfy.Bibliotecas.arithm.Consts.c_pi;
