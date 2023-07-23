a3f34850ca3136a068086362ad5b51cfb9aa6131
Humba01 <humbandroid@gmail.com>
2023-07-23 01:07:06 -0300

    Arrumando laboratório

--- app/index.mjs
+++ app/index.mjs
@@ -1,5 +1,5 @@
 import { wertsfy } from "@humbanew/wertsfy";
 
-wertsfy.Bibliotecas.arithm.Methods.m_arrendonda_para_baixo();
 wertsfy.Bibliotecas.arithm.Consts.c_avogadro;
-wertsfy.Bibliotecas.arithm.Consts.c_pi;
+wertsfy.Bibliotecas.arithm.Methods.m_arrendonda_para_cima();
+
